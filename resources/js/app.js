89793f6f141679a71a7a60b596fd907bc4d018e4
Nima <alireza.khazforoosh@adventistas.org>
2025-01-16 14:21:29 -0300

    adding primevue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,13 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${appName} | ${title}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
@@ -17,6 +19,11 @@ createInertiaApp({
         ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
