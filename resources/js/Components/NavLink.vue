73ba697b829d813ca9a4de7301977af87e3df82d
Nima <alireza.khazforoosh@adventistas.org>
2025-01-17 09:40:40 -0300

    changing existing component's style

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -14,8 +14,8 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out',
+        ? 'flex items-center px-5 py-2 border-indigo-400 text-sm font-medium leading-5 text-indigo-700 bg-indigo-100 rounded-lg focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+        : 'flex items-center px-5 py-2 border-transparent text-sm font-medium leading-5 text-gray-700 dark:text-gray-900 hover:text-gray-700 hover:bg-gray-100 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out dark:bg-white rounded'
 );
 </script>
 
