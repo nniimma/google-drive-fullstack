3097fabd85feb20e10c46193160bba6dc5c4b5eb
Nima <alireza.khazforoosh@adventistas.org>
2025-01-17 10:25:46 -0300

    rendering new route with inertia

---
+++ resources/js/Pages/Files/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <authenticated-layout>
+        hello
+    </authenticated-layout>
+</template>
+
+<script setup>
+  // Imports
+  import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+  import { ref } from 'vue';
+
+  // Uses
+
+  // Refs
+
+  // Props & Emits
+
+  // Computed
+
+  // Methods
+
+  // Hooks
+
+</script>
+
+<style scoped>
+</style>
