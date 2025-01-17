df3f9340ee79add4b1090f2567f5244e86d8c8cf
Nima <alireza.khazforoosh@adventistas.org>
2025-01-17 09:45:19 -0300

    using headless ui for dropdown menu and heroicons for icons

---
+++ resources/js/Components/AuthenticationPage/Navigation/CreateNewDropDown.vue
@@ -0,0 +1,64 @@
+<template>
+    <Menu as="div" class="relative block text-left">
+            <!-- 
+                https://headlessui.com/
+                npm install @headlessui/vue@latest 
+            -->
+        <MenuButton
+            class="flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            Create New
+        </MenuButton>
+
+        <transition enter-active-class="transition duration-100 ease-out" enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
+            <MenuItems
+                class="w-full absolute left-0 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                    <a href="#" @click.prevent="showCreateFolderModal" class="text-gray-700 block px-4 py-2 text-sm">New Folder</a>
+                    </MenuItem>
+                </div>
+                <div class="px-1 py-1">
+                    <!-- <file-upload-menu-item/>
+                    <folder-upload-menu-item/> -->
+                </div>
+            </MenuItems>
+        </transition>
+    </Menu>
+
+    <create-folder-modal v-model="createFolderModal"/>
+</template>
+
+<script setup>
+    // Imports
+    import {
+        Menu,
+        MenuButton,
+        MenuItems,
+        MenuItem
+    } from '@headlessui/vue'
+    import { ref } from 'vue'
+    // import FileUploadMenuItem from '@/Components/AuthenticationPage/Navigation/CreateNewDropDown/FileUploadMenuItem.vue'
+    // import FolderUploadMenuItem from '@/Components/AuthenticationPage/Navigation/CreateNewDropDown/FolderUploadMenuItem.vue'
+
+    // Uses
+
+    // Refs
+    const createFolderModal = ref(false)
+
+    // Props & Emits
+
+    // Computed
+
+    // Methods
+    function showCreateFolderModal(){
+        createFolderModal.value = true
+    }
+
+    // Hooks
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/AuthenticationPage/UserSettingsDropdown.vue
@@ -0,0 +1,79 @@
+<template>
+    <Menu as="div" class="relative inline-block text-left">
+        <div>
+            <!-- 
+                https://headlessui.com/
+                npm install @headlessui/vue@latest 
+             -->
+            <MenuButton
+                class="group inline-flex w-full justify-center rounded-md 0 px-4 py-2 text-sm font-medium text-gray-800 dark:text-gray-700 dark:bg-gray-100 hover:bg-gray-500 hover:dark:text-gray-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+                {{ $page.props.auth.user.name }}
+                <!-- 
+                    https://heroicons.com/
+                    npm install @heroicons/vue
+                  -->
+                <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5 text-gray-800 dark:text-gray-700 group-hover:dark:text-gray-100"
+                    aria-hidden="true" />
+            </MenuButton>
+        </div>
+
+        <transition enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0">
+            <MenuItems
+            class="absolute right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div class="px-1 py-1">
+                <MenuItem v-slot="{ active }">
+                <ResponsiveNavLink class="flex cursor-pointer" :href="route('profile.edit')"
+                    :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">
+
+                    <UserIcon  class="size-4 mr-2" aria-hidden="true" />
+                    Profile
+                </ResponsiveNavLink>
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                <ResponsiveNavLink class="flex cursor-pointer" :href="route('logout')" method="post" as="button"
+                    :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">
+
+                    <ArrowRightOnRectangleIcon  class="size-4 mr-2" aria-hidden="true" />
+
+                    Logout
+                </ResponsiveNavLink >
+                </MenuItem>
+            </div>
+        </MenuItems>
+        </transition>
+    </Menu>
+</template>
+
+
+
+
+<script setup>
+// Imports
+import {
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem
+} from '@headlessui/vue'
+import { ChevronDownIcon, UserIcon, ArrowRightOnRectangleIcon   } from '@heroicons/vue/20/solid';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+
+// Uses
+
+// Refs
+
+// Props & Emits
+
+// Computed
+
+// Methods
+
+// Hooks
+</script>
+
+<style scoped>
+
+</style>
