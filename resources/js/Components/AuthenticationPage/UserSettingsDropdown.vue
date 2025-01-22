0d47b0e44836f0445dce255aee85a61dbaa81ab4
Nima <alireza.khazforoosh@adventistas.org>
2025-01-22 09:51:19 -0300

    update style

--- resources/js/Components/AuthenticationPage/UserSettingsDropdown.vue
+++ resources/js/Components/AuthenticationPage/UserSettingsDropdown.vue
@@ -22,7 +22,7 @@
             leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100"
             leave-to-class="transform scale-95 opacity-0">
             <MenuItems
-            class="absolute right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            class="absolute right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
             <div class="px-1 py-1">
                 <MenuItem v-slot="{ active }">
                 <ResponsiveNavLink class="flex cursor-pointer" :href="route('profile.edit')"
