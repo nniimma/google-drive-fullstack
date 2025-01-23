c1b633007fa6b8ff263743aa437ddad8413d1b14
Nima <alireza.khazforoosh@adventistas.org>
2025-01-23 15:53:20 -0300

    update table to primevue table

--- resources/js/Pages/Files/Index.vue
+++ resources/js/Pages/Files/Index.vue
@@ -12,10 +12,10 @@
                     My Files
                     </Link>
                     <div v-else class="flex items-center">
-                      <i class="pi pi-angle-right"></i>
-                      <Link :href="route('files.index', { folder: ancestor.path })"
-                      class="ml-1 text-sm font-medium text-gray-700 dark:text-white hover:text-blue-100 md:ml-2">
-                        {{ ancestor.name }}
+                        <i class="pi pi-angle-right"></i>
+                        <Link :href="route('files.index', { folder: ancestor.path })"
+                            class="ml-1 text-sm font-medium text-gray-700 dark:text-white hover:text-blue-100 md:ml-2">
+                        {{ ancestor . name }}
                         </Link>
                     </div>
                 </li>
@@ -32,43 +32,23 @@
             </div> -->
         </nav>
 
-        <table class="min-w-full">
-            <thead class="dark:bg-gray-800 border-b dark:border-gray-500 sticky top-0">
-                <tr>
-                    <th class="text-sm font-medium text-gray-900 dark:text-white px-6 py-4 text-left">
-                        Name
-                    </th>
-                    <th class="text-sm font-medium text-gray-900 dark:text-white px-6 py-4 text-left">
-                        Path
-                    </th>
-                    <th class="text-sm font-medium text-gray-900 dark:text-white px-6 py-4 text-left">
-                        Owner
-                    </th>
-                    <th class="text-sm font-medium text-gray-900 dark:text-white px-6 py-4 text-left">
-                        Last Modified
-                    </th>
-                    <th class="text-sm font-medium text-gray-900 dark:text-white px-6 py-4 text-left">
-                        Size
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="file in files.data" :key="file.id" @dblclick="openFolder(file)"
-                    class=" border-b transition duration-300 ease-in-out hover:bg-gray-900 cursor-default">
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300 flex gap-2">
-                            <file-icon :file="file" />
-                            {{ file . name }}
-                        </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300 gap-2">
-                        {{ file . path }}</td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300 gap-2">
-                        {{ file . owner }}</td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300 gap-2">
-                        {{ file . updated_at }}</td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300 gap-2">{{ file . size }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <DataTable v-model:selection="selectedProduct" paginator :rows="10"
+            :rowsPerPageOptions="[5, 10, 20, 50]" :value="files.data" dataKey="id" tableStyle="min-width: 50rem;" @row-dblclick="openFolder" class="cursor-default">
+            <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+            <Column header="Name">
+                <template #body="slotProps">
+                    <div class="flex gap-2">
+                        <file-icon :file="slotProps.data" />
+                        {{ slotProps . data . name }}
+                    </div>
+                </template>
+            </Column>
+            <Column field="path" header="Path"></Column>
+            <Column field="owner" header="Owner"></Column>
+            <Column field="updated_at" header="Last Modified"></Column>
+            <Column field="size" header="Size"></Column>
+        </DataTable>
+
         <div v-if="!files.data.length" class="py-8 text-center text-md text-gray-400">
             There is no data in this folder.
         </div>
@@ -90,10 +70,13 @@
         StarIcon as StarSolidIcon,
     } from '@heroicons/vue/20/solid'
     import FileIcon from '@/Components/Files/FileIcon.vue';
+    import DataTable from 'primevue/datatable';
+    import Column from 'primevue/column';
 
     // Uses
 
     // Refs
+    const selectedProduct = ref([]);
 
     // Props & Emits
     const props = defineProps({
@@ -106,13 +89,13 @@
 
     // Methods
     const openFolder = (file) => {
-        if (!file.is_folder) {
-            return
+        if (!file.data.is_folder) {
+            return;
         }
 
         router.visit(route('files.index', {
-            folder: file.path
-        }))
+            folder: file.data.path
+        }));
     }
 
     // Hooks
