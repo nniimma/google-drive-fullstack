d2651b86409096cee38ce2b86493266b282be43b
Nima <alireza.khazforoosh@adventistas.org>
2025-01-23 10:39:18 -0300

    adding register to login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -86,6 +86,12 @@ const submit = () => {
                 >
                     Forgot your password?
                 </Link>
+                <Link
+                    :href="route('register')"
+                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800 ms-2"
+                >
+                    Register
+                </Link>
 
                 <PrimaryButton
                     class="ms-4"
